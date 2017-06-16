ee5691a7bea9a98b92980d4611df8ac105e6f4e4
jojoshuai <winterkid@foxmail.com>
2017-06-16 12:44:43 +0800

    sperate view and template

--- basicphp/View.php
+++ basicphp/View.php
@@ -12,7 +12,44 @@ class View
     public function render($viewPath)
     {
         //extract($this->variables);
+        $activeHeaderAnchor = $this->getAnchor($viewPath);
+        include (VIEW_PATH . DIRECTORY_SEPARATOR . 'Base/Header.php');
         include ($viewPath);
+        include (VIEW_PATH . DIRECTORY_SEPARATOR . 'Base/Footer.php');
+    }
+    
+    private function getAnchor($viewPath)
+    {
+        if (empty($viewPath)) {
+            return 0;
+        }
+        //VIEW_PATH . DIRECTORY_SEPARATOR . $controllerName . DIRECTORY_SEPARATOR . $actionName . '.php';
+        $strSplits = explode(DIRECTORY_SEPARATOR, $viewPath);
+        if (count($strSplits) <= 2) {
+            return 0;
+        }
+        $anchor = 0;
+        $controllerName =  $strSplits[count($strSplits) - 2];
+        switch ($controllerName) {
+            case 'Index':
+                $anchor = 0;
+                break;
+            case 'Acticle':
+                $anchor = 1;
+                break;
+            case 'Picture':
+                $anchor = 2;
+                break;
+            case 'Message':
+                $anchor = 3;
+                break;
+            case 'Me':
+                $anchor = 4;
+                break;
+            default:
+                $anchor = 0;
+        }
+        return $anchor;
     }
 
 
